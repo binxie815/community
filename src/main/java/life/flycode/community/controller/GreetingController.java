1f3daf996c1e7b40f55bc96af4d4fead720bb0c6
xiebin <1728624162@qq.com>
2020-01-01 15:53:27 +0800

    add index and bootstrap

--- src/main/java/life/flycode/community/controller/GreetingController.java
+++ src/main/java/life/flycode/community/controller/GreetingController.java
@@ -1,9 +1,7 @@
 package life.flycode.community.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @Auther
@@ -13,9 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class GreetingController {
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @GetMapping("/")
+    public String greeting() {
+        return "index";
     }
 }
